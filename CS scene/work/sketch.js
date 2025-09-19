c405d54e4facdaf13a4facdcc4872176c1a3db3c
Wang7004898 <7004898@spsd.sk.ca>
2025-09-19 13:52:40 -0600

    9.19 last up(...last)

--- CS scene/work/sketch.js
+++ CS scene/work/sketch.js
@@ -2,30 +2,29 @@
 // Tony
 // Sep, 17, 2025
 //
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
 
-let x = 200, y = 200;
-let size = 50;
-let currentBack = 0;
+let x = 200, y = 200;// position
+let size = 50;    //protangonist Size
+let currentBack = 0; // backgroud State
 
 function setup(){
   createCanvas(400,400);
 }
  
 function draw() {
+   // backgroud color change
   if (currentBack == 0) background(200, 220, 255);
   if (currentBack == 1) background(255, 255, 150);
   if (currentBack == 2) background(200, 255, 200);
   if (currentBack == 3) background(240, 240, 180);
-  // draw
+  // Scene with 3 primitives
   rect(50, 300, 300, 50);
   ellipse(100, 100, 80, 80);
   line(0, 0, 400, 400);
-  //protangon
+  //protangonist
   fill(255, 150, 0);
   ellipse(x, y, size, size);
-
+  //My name
   fill(0);
   textSize(16);
   text("By Tony", 10, heght - 10);
